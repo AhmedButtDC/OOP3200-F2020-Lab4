04f8b5c7ca1d2727d24ac19d3f5d62f299e7947b
Ahmed Butt <TypicalNewb@outlook.com>
2020-10-07 14:06:45 -0400

    Add project files.

---
+++ OOP3200-F2020-Lab4/StandardDeck.h
@@ -0,0 +1,40 @@
+/** OOP3200. F2020. StandardDeck.h
+ *
+ *  In this lab you will create your own collection class called StandardDeck, which
+ *	will 'contain' objects of the PlayingCard class.  The intent of the class is to
+ *	represent a standard deck of 52 cards that might be useful in a card game type
+ *	application.  
+ *
+ *	@author		Thom MacDonald
+ *	@author		Tom Tsiliopoulos
+ *	@version	2020.10
+ *	@since		Jul 2018
+ *	@see		Bronson, G. (2012). A First Book of C++ (4th ed.). Boston, MA: Course Technology.
+*/
+
+#pragma once
+#ifndef __STANDARD_DECK__
+#define __STANDARD_DECK__
+
+#include "PlayingCard.h"
+
+class StandardDeck
+{
+public:
+	// Initialization:
+	StandardDeck();
+
+	// Rule of three:
+	~StandardDeck();
+
+
+
+	// Accessors:
+
+	// Mutators:
+private:
+
+};
+
+#endif /* defined (__STANDARD_DECK__) */
+
